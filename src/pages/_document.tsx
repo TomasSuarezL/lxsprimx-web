7635240d8e23c6f1665c5cc80e59abed46aee274
Tomas Suarez Lissi <slissi@cariverplate.com.ar>
2022-05-31 10:07:38 -0300

    Fix font importing error

---
+++ src/pages/_document.tsx
@@ -0,0 +1,17 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        {/* <link rel="preload" href="/fonts/BRICE-BLACK.otf" as="font" type="font/otf" crossOrigin="" />
+        <link rel="preload" href="/fonts/BRICE-BLACKSEMIEXPANDED.otf" as="font" type="font/otf" crossOrigin="" /> */}
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
+
