bf0ad95e410760d28b060a5471d477ef10041dc8
Tomas Suarez Lissi <slissi@cariverplate.com.ar>
2022-05-31 09:23:16 -0300

    Add custom font (Brice)

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -8,12 +8,12 @@ export const Home = () => {
       <div className="triangle-container z-10 absolute -bottom-1/3 left-0 w-full h-[150%] ml-auto mr-auto text-center">
         <div className="background-container absolute w-full h-full">{/* <Background /> */}</div>
         <Triangle />
-        <div className="absolute flex items-center justify-center top-0 left-0 w-full h-3/4 px-4 ml-auto mr-auto text-center">
-          {/* <h1 className="text-sky-400 text-6xl lg:text-9xl font-black">Lxs</h1>
-          <h1 className="text-amber-500 text-6xl lg:text-9xl font-black">Primxs</h1> */}
-          <div className="h-96 w-[32rem]">
+        <div className="absolute flex items-center flex-col justify-center top-0 left-0 w-full h-3/4 px-4 ml-auto mr-auto text-center">
+          <h1 className="text-sky-400 text-6xl lg:text-9xl font-black">LXS</h1>
+          <h1 className="text-amber-500 text-6xl lg:text-9xl font-black">PRIMXS</h1>
+          {/* <div className="h-96 w-[32rem]">
             <Image src="/Isologo 1.png" height={700} width={700} objectFit="cover"></Image>
-          </div>
+          </div> */}
         </div>
       </div>
     </>
--- src/components/sections/Musica.tsx
+++ src/components/sections/Musica.tsx
@@ -1,7 +1,7 @@
 export const Musica = () => {
   return (
     <div className="absolute w-full h-full flex items-center justify-center">
-      <h1 className="text-sky-400 text-6xl lg:text-9xl font-black">Musicx</h1>
+      <h1 className="text-sky-400 text-6xl lg:text-9xl font-bold">Musicx</h1>
     </div>
   );
 };
