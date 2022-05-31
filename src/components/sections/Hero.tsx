971cfa50407a6f2a34351dba0d84d15903893e65
Tomas Suarez Lissi <slissi@cariverplate.com.ar>
2022-05-31 09:24:05 -0300

    Add custom font (Brice)

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -9,7 +9,7 @@ export const Home = () => {
         <div className="background-container absolute w-full h-full">{/* <Background /> */}</div>
         <Triangle />
         <div className="absolute flex items-center flex-col justify-center top-0 left-0 w-full h-3/4 px-4 ml-auto mr-auto text-center">
-          <h1 className="text-sky-400 text-6xl lg:text-9xl font-black">LXS</h1>
+          <h1 className="text-amber-500 text-6xl lg:text-9xl font-black">LXS</h1>
           <h1 className="text-amber-500 text-6xl lg:text-9xl font-black">PRIMXS</h1>
           {/* <div className="h-96 w-[32rem]">
             <Image src="/Isologo 1.png" height={700} width={700} objectFit="cover"></Image>
