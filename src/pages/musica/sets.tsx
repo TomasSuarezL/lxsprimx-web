9978f8208458a1b4e5f582fc3477f76a34693227
Tomas Suarez Lissi <slissi@cariverplate.com.ar>
2022-06-21 11:46:43 -0300

    apunto por set en soundcloud

--- src/pages/musica/sets.tsx
+++ src/pages/musica/sets.tsx
@@ -25,12 +25,12 @@ const SetsPage = () => {
         <div className="flex flex-col w-full h-[65vh] overflow-auto items-center p-4">
           <ul className="flex flex-col lg:flex-row">
             <li className="relative p-4 m-2 bg-amber-500 hover:bg-amber-600 flex flex-column items-end w-40 lg:w-64 h-40 lg:h-64 transition-colors cursor-pointer">
-              <a href="https://soundcloud.com/lxsprimxs">
+              <a href="https://soundcloud.com/lxsprimxs/lxs-primxs-en-california-klub-16-6">
                 <Image src="/4.png" layout="fill" objectFit="cover"></Image>
-                <h2 className="relative bottom-0 text-black lg:text-2xl font-extrabold">Negro</h2>
+                <h2 className="relative bottom-0 text-black lg:text-2xl font-extrabold">California Club 16-6</h2>
               </a>
             </li>
-            <li className="relative p-4 m-2 bg-amber-500 hover:bg-amber-600 flex flex-column items-end w-40 lg:w-64 h-40 lg:h-64 transition-colors cursor-pointer">
+            {/* <li className="relative p-4 m-2 bg-amber-500 hover:bg-amber-600 flex flex-column items-end w-40 lg:w-64 h-40 lg:h-64 transition-colors cursor-pointer">
               <a href="https://soundcloud.com/lxsprimxs">
                 <Image src="/5.png" layout="fill" objectFit="cover"></Image>
                 <h2 className="relative bottom-0 text-black lg:text-2xl font-extrabold">Cabeza</h2>
@@ -41,7 +41,7 @@ const SetsPage = () => {
                 <Image src="/7.png" layout="fill" objectFit="cover"></Image>
                 <h2 className="relative bottom-0 text-black lg:text-2xl font-extrabold">Termo</h2>
               </a>
-            </li>
+            </li> */}
           </ul>
         </div>
       </motion.div>
