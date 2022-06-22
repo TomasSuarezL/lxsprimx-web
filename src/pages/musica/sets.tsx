cae9a211c71c5ca579c59913a2b366b18b3337a0
Tomas Suarez Lissi <slissi@cariverplate.com.ar>
2022-06-22 08:39:41 -0300

    Tweak portada set

--- src/pages/musica/sets.tsx
+++ src/pages/musica/sets.tsx
@@ -24,10 +24,13 @@ const SetsPage = () => {
         <MusicaNav />
         <div className="flex flex-col w-full h-[65vh] overflow-auto items-center p-4">
           <ul className="flex flex-col lg:flex-row">
-            <li className="relative p-4 m-2 bg-amber-500 hover:bg-amber-600 flex flex-column items-end w-40 lg:w-64 h-40 lg:h-64 transition-colors cursor-pointer">
-              <a href="https://soundcloud.com/lxsprimxs/lxs-primxs-en-california-klub-16-6">
+            <li className="relative m-2 bg-amber-500 hover:bg-amber-600 flex flex-column items-end w-60 sm:w-64 lg:w-72 h-48 sm:h-56 lg:h-64 transition-colors duration-300 cursor-pointer">
+              <a className="w-full" href="https://soundcloud.com/lxsprimxs/lxs-primxs-en-california-klub-16-6">
                 <Image src="/5.png" layout="fill" objectFit="cover"></Image>
-                <h2 className="relative bottom-0 text-black lg:text-2xl font-extrabold">California Club 16-6</h2>
+                <h2 className="relative w-full bottom-0 m-0 p-3 pt-20 bg-gradient-to-t from-amber-200 to-transparent text-black lg:text-xl font-extrabold">
+                  California Klub <br />
+                  16-6
+                </h2>
               </a>
             </li>
             {/* <li className="relative p-4 m-2 bg-amber-500 hover:bg-amber-600 flex flex-column items-end w-40 lg:w-64 h-40 lg:h-64 transition-colors cursor-pointer">
