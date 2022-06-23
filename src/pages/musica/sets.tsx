f64b629e8e7cf3fe7ff84731f19986d62182ac34
Tomas Suarez Lissi <slissi@cariverplate.com.ar>
2022-06-23 09:26:11 -0300

    SC player react-player

--- src/pages/musica/sets.tsx
+++ src/pages/musica/sets.tsx
@@ -3,6 +3,9 @@ import { motion } from "framer-motion";
 import { HomeNav } from "../../components/layout/HomeNav";
 import { Layout } from "../../components/layout/Layout";
 import { MusicaNav } from "../../components/sections/musica/MusicaNav";
+import { SoundcloudPlayer } from "../../components/player/SoundcloudPlayer";
+import AudioPlayer from "react-h5-audio-player";
+import ReactPlayer from "react-player";
 
 const SetsPage = () => {
   return (
@@ -24,7 +27,7 @@ const SetsPage = () => {
         <MusicaNav />
         <div className="flex flex-col w-full h-[65vh] overflow-auto items-center p-4">
           <ul className="flex flex-col lg:flex-row">
-            <li className="relative m-2 bg-amber-500 hover:bg-amber-600 flex flex-column items-end w-60 sm:w-64 lg:w-72 h-48 sm:h-56 lg:h-64 transition-colors duration-300 cursor-pointer">
+            {/* <li className="relative m-2 bg-amber-500 hover:bg-amber-600 flex flex-column items-end w-60 sm:w-64 lg:w-72 h-48 sm:h-56 lg:h-64 transition-colors duration-300 cursor-pointer">
               <a className="w-full" href="https://soundcloud.com/lxsprimxs/lxs-primxs-en-california-klub-16-6">
                 <Image src="/5.png" layout="fill" objectFit="cover"></Image>
                 <h2 className="relative w-full bottom-0 m-0 p-3 pt-20 bg-gradient-to-t from-amber-200 to-transparent text-black lg:text-xl font-extrabold">
@@ -32,7 +35,11 @@ const SetsPage = () => {
                   16-6
                 </h2>
               </a>
+            </li> */}
+            <li className="relative m-2 bg-amber-500 hover:bg-amber-600 flex flex-column items-end  transition-colors duration-300 cursor-pointer">
+              <ReactPlayer url="https://soundcloud.com/lxsprimxs/lxs-primxs-en-california-klub-16-6" />
             </li>
+
             {/* <li className="relative p-4 m-2 bg-amber-500 hover:bg-amber-600 flex flex-column items-end w-40 lg:w-64 h-40 lg:h-64 transition-colors cursor-pointer">
               <a href="https://soundcloud.com/lxsprimxs">
                 <Image src="/5.png" layout="fill" objectFit="cover"></Image>
