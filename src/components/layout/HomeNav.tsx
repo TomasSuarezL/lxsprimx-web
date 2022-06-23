d18b5fc1ea766dfe1346546793246b3f95f82ff1
Tomas Suarez Lissi <slissi@cariverplate.com.ar>
2022-06-23 08:50:31 -0300

    Agrego soundcloud y YT a redes

--- src/components/layout/HomeNav.tsx
+++ src/components/layout/HomeNav.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export const HomeNav = () => {
   return (
-    <nav className="top-0 absolute z-50 w-full flex flex-wrap items-center justify-center px-2 py-6 mt-2 ">
+    <nav className="top-0 absolute z-50 w-full flex flex-wrap items-center justify-center px-2 py-6 bg-black">
       <Link href="/">
         <a className="my-3 h-12 flex items-center">
           <Image src="/5B.png" layout="fill" objectFit="contain"></Image>
--- src/components/sections/Redes.tsx
+++ src/components/sections/Redes.tsx
@@ -1,22 +1,38 @@
 import { EmailIcon } from "../../assets/Email";
 import { InstagramIcon } from "../../assets/Instagram";
+import { SoundcloudIcon } from "../../assets/Soundcloud";
+import { YouTubeIcon } from "../../assets/Youtube";
 
 export const Redes = () => {
   return (
-    <div className="absolute w-full h-full py-32 flex-col lg:flex-row flex items-center lg:justify-center">
+    <div className="absolute overflow-auto w-full h-full py-32 flex-col lg:flex-wrap lg:flex-row flex items-center lg:justify-center">
       <a
-        className="redes-link w-full flex flex-col items-center justify-center m-3 lg:p-4 lg:m-8 text-amber-500 text-sm sm:text-lg lg:text-2xl font-black"
+        className="redes-link w-full flex flex-col items-center justify-center m-3 lg:p-4 lg:m-8 text-amber-500 text-sm sm:text-lg  font-black"
         href="https://www.instagram.com/lxs__primxs/ "
       >
         <InstagramIcon />
         @lxs__primxs
       </a>
       <a
-        className="redes-link w-full flex flex-col items-center justify-center m-3 lg:p-4 lg:m-8 text-amber-500  text-sm sm:text-lg lg:text-2xl font-black"
+        className="redes-link w-full flex flex-col items-center justify-center m-3 lg:p-4 lg:m-8 text-amber-500  text-sm sm:text-lg font-black"
         href="mailto:lxsprimxs909@gmail.com"
       >
         <EmailIcon />
         <span>lxsprimxs909@gmail.com</span>
+      </a>
+      <a
+        className="redes-link w-full flex flex-col items-center justify-center m-3 lg:p-4 lg:m-8 text-amber-500 text-sm sm:text-lg  font-black"
+        href="https://www.youtube.com/channel/UCnbiRo5b05-os-yOILyWXCQ"
+      >
+        <YouTubeIcon />
+        Lxs Primxs
+      </a>
+      <a
+        className="redes-link w-full flex flex-col items-center justify-center m-3 lg:p-4 lg:m-8 text-amber-500  text-sm sm:text-lg  font-black"
+        href="https://soundcloud.com/lxsprimxs"
+      >
+        <SoundcloudIcon />
+        <span>lxsprimxs</span>
       </a>
     </div>
   );
